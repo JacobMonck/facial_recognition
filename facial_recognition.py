0104af6660cff37bb61230893bd6448f0db0dde9
Slushs <jakeamonck@gmail.com>
2021-06-04 19:23:05 -0400

    Remove profile cascade

--- facial_recognition.py
+++ facial_recognition.py
@@ -2,7 +2,6 @@ import numpy as np
 
 import cv2
 
-profile_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_profileface.xml")
 face_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt.xml")
 
 
